b218de1b4f5d826b2542759eefdd4986f5629662
Mateo <quotterimateo@gmail.com>
2024-08-19 11:55:37 -0300

    security config added and postman works correctly on basic users endpoints

---
+++ PIBackend/src/main/java/com/PI/Back/PIBackend/config/SegurityConfig.java
@@ -0,0 +1,30 @@
+package com.PI.Back.PIBackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SegurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authRequest ->
+                        authRequest
+                                .requestMatchers("/usuarios").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .build();
+    }
+}
+
