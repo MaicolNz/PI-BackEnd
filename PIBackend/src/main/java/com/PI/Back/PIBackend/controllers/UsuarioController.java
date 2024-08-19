35d8fabed176b9b67e4ea88e1f3b8601862400c7
Mateo <quotterimateo@gmail.com>
2024-08-18 23:01:58 -0300

    trying to do basic implementations on usercontroller and userservice

--- PIBackend/src/main/java/com/PI/Back/PIBackend/controllers/UsuarioController.java
+++ PIBackend/src/main/java/com/PI/Back/PIBackend/controllers/UsuarioController.java
@@ -3,11 +3,15 @@ package com.PI.Back.PIBackend.controllers;
 
 import com.PI.Back.PIBackend.entity.Usuario;
 import com.PI.Back.PIBackend.services.UsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UsuarioController {
+    private final Logger LOGGER = LoggerFactory.getLogger(UsuarioController.class);
+
     @Autowired
     final UsuarioService usuarioService;
 
@@ -19,6 +23,7 @@ public class UsuarioController {
     @GetMapping("/usuarios")
     @ResponseBody
     public String listarUsuarios(){
+        LOGGER.info("ACA ESTAMOS");
         return usuarioService.listarUsuarios();
     }
     @PostMapping("/usuarios")
--- PIBackend/src/main/java/com/PI/Back/PIBackend/services/UsuarioService.java
+++ PIBackend/src/main/java/com/PI/Back/PIBackend/services/UsuarioService.java
@@ -3,16 +3,19 @@ package com.PI.Back.PIBackend.services;
 
 import com.PI.Back.PIBackend.entity.Usuario;
 import com.PI.Back.PIBackend.repository.UsuarioRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
-
+    private final Logger LOGGER = LoggerFactory.getLogger(UsuarioService.class);
     @Autowired
     UsuarioRepository usuarioRepository;
 
     public String listarUsuarios() {
+        LOGGER.info("ACA ESTAMOS");
         return "Usuarios: bla bla";
     }
     public Usuario crearUsuario(Usuario usuario) {
